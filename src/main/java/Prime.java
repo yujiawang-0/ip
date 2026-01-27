c936f0900bf8fc8bf082069aab01deebf125f0be
skylar <yujiawang135312@gmail.com>
2026-01-27 10:16:41 +0800

    Level 0: Rename, greet, exit

---
+++ src/main/java/Prime.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+
+public class Prime {
+    public static void main(String[] args) {
+        String line = "________________________________________________";
+        String greet = "I AM OPTIMUS PRIME.\nWhat can I do for you?";
+        String bye = "Goodbye.";
+
+        System.out.println(line);
+        System.out.println(greet);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+    }
+}
