7d2681d0329b60db99245a9d90ec0e69923ace03
skylar <yujiawang135312@gmail.com>
2026-01-27 11:11:48 +0800

    completed level 2: able to add items to log and list them

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -1,16 +1,48 @@
-import java.io.Console;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Prime {
-    public static void main(String[] args) {
-        String line = "________________________________________________";
-        String greet = "I AM OPTIMUS PRIME.\nWhat can I do for you?";
-        String bye = "Goodbye.";
+    public static String line = "________________________________________________";
+    public static String greet = "I AM OPTIMUS PRIME.\nWhat can I do for you?";
+    public static String bye = "Goodbye.";
 
-        // prints Prime's greeting 
+    private static ArrayList<String> log = new ArrayList<>();
+
+    // prints greeting when called
+    public static void hello() {
         System.out.println(line);
         System.out.println(greet);
         System.out.println(line);
+    }
+
+    // prints goodbye when called
+    public static void goodbye() {
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+    }
+
+    // adds item to log and repeats what has been added
+    public static void add(String item) {
+        log.add(item);
+        System.out.println(line);
+        System.out.println("Added to list: " + item);
+        System.out.println(line);
+    }
+
+    // prints out list
+    public static void list() {
+        int i = log.size();
+        System.out.println(line);
+        for (int a = 0; a < i; a++) {
+            System.out.println((a+1)+ ". " + log.get(a));
+        }
+        System.out.println(line);
+    }
+
+    public static void main(String[] args) {
+        hello();
 
         Scanner s = new Scanner(System.in);
 
@@ -19,23 +51,18 @@ public class Prime {
             String command = s.nextLine();
             
             if (command.equalsIgnoreCase("bye")) {
-                System.out.println(line);
-                System.out.println(bye);
-                System.out.println(line);
+                // stop the commands when "bye" is inputted
+                goodbye();
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                // stop the commands when "bye" is inputted
+                list();
             } else {
-                System.out.println(line);
-                System.out.println("You said: " + command);
-                System.out.println(line);
+                // else, add item to log
+                add(command);
             }
         }
 
         s.close();
-
-        
-        
-
-        
-        
     }
 }
