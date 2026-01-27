e983cd9cb9007f9405e1883f78ebf5b23b34aa9b
skylar <yujiawang135312@gmail.com>
2026-01-27 10:50:15 +0800

    completed Level-1: Echo

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
@@ -6,10 +7,35 @@ public class Prime {
         String greet = "I AM OPTIMUS PRIME.\nWhat can I do for you?";
         String bye = "Goodbye.";
 
+        // prints Prime's greeting 
         System.out.println(line);
         System.out.println(greet);
         System.out.println(line);
-        System.out.println(bye);
-        System.out.println(line);
+
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Please, give me an instruction: ");
+            String command = s.nextLine();
+            
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println(bye);
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println("You said: " + command);
+                System.out.println(line);
+            }
+        }
+
+        s.close();
+
+        
+        
+
+        
+        
     }
 }
