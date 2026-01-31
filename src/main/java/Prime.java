6195865811af30d51098725e9fb57b1f2ce748d0
skylar <yujiawang135312@gmail.com>
2026-01-31 23:52:56 +0800

    add local storage functionality Level-7

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -2,135 +2,46 @@ import java.util.Scanner;
 
 public class Prime {
     private static final Log log = new Log();
+    private static Storage storage;
 
     public static void main(String[] args) {
-        Ui.showHello();
+        try {
+            storage = new Storage();
 
-        Scanner s = new Scanner(System.in);
-        boolean isRunning = true;
-
-        while (isRunning) {
-            try {
-                Ui.showMessage("Please, give me an instruction: ");
-                String input = s.nextLine().trim();
-                Ui.showLine();
-
-                isRunning = PrimeParser.parse(input, log);
-
-                Ui.showLine();
-
-            } catch (PrimeException e) {
-                Ui.showError(e.getMessage());
-                Ui.showLine();
+            // Load saved tasks
+            for (ToDo task : storage.loadData()) {
+                log.add(task);
             }
+
+            Ui.showHello();
+
+            Scanner s = new Scanner(System.in);
+            boolean isRunning = true;
+
+            while (isRunning) {
+                try {
+                    Ui.showMessage("Please, give me an instruction: ");
+                    String input = s.nextLine().trim();
+                    Ui.showLine();
+
+                    isRunning = PrimeParser.parse(input, log);
+
+                    // save the data after every command by the user
+                    storage.saveData(log.getAll());
+
+                    Ui.showLine();
+
+                } catch (PrimeException e) {
+                    Ui.showError(e.getMessage());
+                    Ui.showLine();
+                }
+            }
+
+            s.close();
+
+        } catch (Exception e) {
+            Ui.showError(e.getMessage());
         }
-
-        s.close();
-
-        // while (true) {
-        //     System.out.print("Please, give me an instruction: ");
-        //     String input = s.nextLine().trim();
-        //     System.out.println(line);
-
-        //     if (input.isEmpty()) {
-        //         System.out.println("Hmm.. you didn't say anything...");
-        //         System.out.println(line);
-        //     }
-
-        //     String[] parts = input.split("\\s+", 2);
-        //     String command = parts[0].toLowerCase();
-        //     String rest = parts.length > 1 ? parts[1] : "";
-            
-        //     if (command.equalsIgnoreCase("bye")) {
-        //         // stop the commands when "bye" is inputted
-        //         goodbye();
-        //         System.out.println(line);
-        //         break;
-        //     } else if (command.equalsIgnoreCase("list")) {
-        //         // list the current items
-        //         logSize();
-        //         list();
-        //         System.out.println(line);
-        //     } else if (command.equalsIgnoreCase("mark")) {
-        //         // input should be "mark [int]"
-        //         if (parts.length < 2) {
-        //             System.out.println("Please tell me which task to mark.");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         int index = Integer.parseInt(parts[1]);
-        //         mark(index);
-        //     } else if (command.equalsIgnoreCase("unmark")) {
-        //         // input should be "unmark [int]"
-        //         if (parts.length < 2) {
-        //             System.out.println("Please tell me which task to unmark.");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         int index = Integer.parseInt(parts[1]);
-        //         unmark(index);
-        //     } else if (command.equalsIgnoreCase("todo")) {
-        //         // input is a todo
-        //         // todos need task descriptions
-        //         if (rest.isEmpty()) {
-        //             System.out.println("Please give me a task description.");
-        //             System.out.println(line);
-        //             continue;
-        //         } 
-        //         addTodo(rest);
-        //         logSize();
-        //         System.out.println(line);
-                
-        //     } else if (command.equalsIgnoreCase("deadline")) {
-        //         // input is a deadline
-        //         // deadlines need task descriptions and a deadline 
-        //         if (rest.isEmpty()) {
-        //             System.out.println("I am sorry, but this instruction is incomplete.");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         String[] dueStrings = rest.split("/by ", 2);
-        //         if (dueStrings.length != 2) {
-        //             System.out.println("Please provide me with the task description and the due date.");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         addDeadline(dueStrings[0], dueStrings[1]);
-        //         logSize();
-        //         System.out.println(line);
-        //     } 
-        //     else if (command.equalsIgnoreCase("event")) {
-        //         // input is a event
-        //         // events need task desc, start and end time
-        //         if (rest.isEmpty()) {
-        //             System.out.println("I am sorry, but this instruction is incomplete");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         String[] timingsArray = rest.split("/from ", 2);
-        //         if (timingsArray.length != 2) {
-        //             System.out.println("Please provide me with the task description, start and end times.");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         String timings = timingsArray[1];
-        //         String[] dates = timings.split("/to ", 2);
-        //         if (dates.length != 2) {
-        //             System.out.println("Are you missing the start or end times? They must be in the correct order.");
-        //             System.out.println(line);
-        //             continue;
-        //         }
-        //         addEvent(timingsArray[0].trim(), dates[0], dates[1]);
-        //         logSize();
-        //         System.out.println(line);
-        //     } else {
-        //         // else, say that there is an error
-        //         System.out.println("I am sorry, I do not recognise this instruction...");
-        //         System.out.println(line);
-        //         continue;
-        //     }
-        // }
-
-        // s.close();
+        
     }
 }
---
+++ src/main/java/Storage.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Storage {
+    private static final String DIR_PATH = "/data";
+    private static final String FILE_PATH = "data/prime.txt";
+
+    public Storage() throws PrimeException{
+        try {
+            File file = new File(FILE_PATH);
+            file.getParentFile().mkdir(); // create ./data if missing
+            file.createNewFile();   // create the storage file if missing
+        } catch (IOException e) {
+            throw new PrimeException("I am unable to initialise storage:" + e.getMessage());
+        }
+    }
+
+    public void saveData(ArrayList<ToDo> log) throws PrimeException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            for (ToDo task : log) {
+                bw.write(task.toFileString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            throw new PrimeException("Apologies, there is an error in saving log:" + e.getMessage());
+        }
+    }
+
+
+    public ArrayList<ToDo> loadData() throws PrimeException{
+        ArrayList<ToDo> tasks = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                tasks.add(parseLine(line));
+            }
+        } catch (IOException e) {
+            throw new PrimeException("Apologies, there is an error reading data:" + e.getMessage());
+        }
+
+        return tasks;
+    }
+
+
+    private ToDo parseLine(String line) throws PrimeException{
+        try {
+            String[] parts = line.split(" \\| ");
+            boolean isDone = parts[1].equals("1");
+
+            switch (parts[0]) {
+            case "T":
+                return new ToDo(isDone, parts[2]);
+            
+            case "D":
+                return new Deadline(isDone, parts[2], parts[3]);
+            
+            case "E":
+                return new Event(isDone, parts[2], parts[3], parts[4]);    
+
+            default:
+                throw new PrimeException("There seems to be a corrupted data file...");
+            }
+        } catch (Exception e) {
+            throw new PrimeException("There seems to be a corrupted data file...");
+        }
+        
+    }
+
+}
+
+
+
+// public class FilesClassDemo {
+
+//     public static void main(String[] args) throws IOException{
+//         Files.copy(Paths.get("data/fruits.txt"), Paths.get("temp/fruits2.txt"));
+//         Files.delete(Paths.get("temp/fruits2.txt"));
+//     }
+
+// }
+
+
+// public class FileWritingDemo {
+
+//     private static void writeToFile(String filePath, String textToAdd) throws IOException {
+//         FileWriter fw = new FileWriter(filePath);
+//         fw.write(textToAdd);
+//         fw.close();
+//     }
+
+//     public static void main(String[] args) {
+//         String file2 = "temp/lines.txt";
+//         try {
+//             writeToFile(file2, "first line" + System.lineSeparator() + "second line");
+//         } catch (IOException e) {
+//             System.out.println("Something went wrong: " + e.getMessage());
+//         }
+//     }
+
+// }
